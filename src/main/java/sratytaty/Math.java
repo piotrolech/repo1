f0180ba7ddf0081d9bffda860b57d33b574e6f8f
ulan1989 <ulan1989@o2.pl>
2017-01-05 19:52:35 +0100

    allah akbar!

---
+++ src/main/java/sratytaty/Math.java
@@ -0,0 +1,13 @@
+package sratytaty;
+
+import java.util.List;
+
+/**
+ * Created by RENT on 2017-01-05.
+ */
+public class Math {
+    public static List<Factors> getFactors(int number) {
+
+        return null;
+    }
+}
