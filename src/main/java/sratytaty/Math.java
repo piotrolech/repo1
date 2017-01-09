d801a2795b22f62d96621b28aab4eae9bb39d40a
Piotr Olech <piotrolech@wp.pl>
2017-01-09 18:32:01 +0100

    Working

--- src/main/java/sratytaty/Math.java
+++ src/main/java/sratytaty/Math.java
@@ -10,7 +10,7 @@ public class Math {
     public static List<Factors> getFactors(int number) {
         List<Factors> factors = new LinkedList<Factors>();
 
-        for (int i = 1; i < number+1; i++) {
+        for (int i = 1; i <= number; i++) {
             if(number%i == 0){
                 Factors factor = new Factors(0,0);
                 factor.setX(i);
